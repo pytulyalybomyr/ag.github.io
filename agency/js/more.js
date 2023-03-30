c870957a5bdd1f537ea7cfd44968c68f9328933b
pytulyalybomyr <118480061+pytulyalybomyr@users.noreply.github.com>
2023-03-30 23:04:56 +0300

    Delete agency directory

--- agency/js/more.js
+++
@@ -1,7 +0,0 @@
-const more = document.querySelector(".more-button")
-const container = document.querySelector(".more")
-
-more.addEventListener("click", function() {
-    more.style.display = "none"
-    container.style.display = "flex"
-})
